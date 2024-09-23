24b4e7144621c029e62f0f5e4b9cb314051a4379
DhanushJadala <dhanushj6560@gmail.com>
2024-09-23 11:31:24 +0530

    uploaded java file
    uploaded java factorial  file in the opensource respository

---
+++ java/Factorial.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Factorial{
+	public static void main(String args[]){
+		int n,i,fact=1;
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the number");
+		n=s.nextInt();
+		for(i=1;i<=n;i++){
+			fact=fact*i;
+		}
+		System.out.println(fact);
+	}
+}
